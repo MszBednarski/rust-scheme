ad43983deb1395c1788afdbd1bcc889e0901374e
MszBednarski <msz.bednarski@gmail.com>
2021-05-12 17:35:36 +0200

    repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate rustyline;
+
 fn main() {
-    println!("Hello, world!");
+    let mut done = false;
+    let mut reader = rustyline::Editor::<()>::new();
+    while !done {
+        match reader.readline("> ") {
+            Ok(line) =>
+                if line.trim() == "(exit)" {
+                    done = true;
+                } else {
+                    println!("{ }", line);
+                },
+            Err(e) => match e {
+                rustyline::error::ReadlineError::Eof => done = true,
+                rustyline::error::ReadlineError::Interrupted => done = true,
+                _ => println!("Couldn't readline. Error was: {}", e),
+            }
+        }
+    }
 }
